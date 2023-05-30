817845c10bcd4fef768c8fa14c54bc3431060b46
Mario <canalessanchezmario@gmail.com>
2023-05-30 20:30:42 +0200

    Se añade el componente Mensaje, se agrega la funcionalidad y se importa en NuevoPresupuesto. Se añaden validaciones del formulario en NuevoPresupuesto

--- control-gastos/src/Components/NuevoPresupuesto.jsx
+++ control-gastos/src/Components/NuevoPresupuesto.jsx
@@ -1,11 +1,22 @@
 import { useEffect, useState } from "react"
+import { Mensaje } from "./Mensaje"
 
 function NuevoPresupuesto({presupuesto, setPresupuesto}) {
+    const [mensaje, setMensaje] =useState('')
+    const handlePresupuesto = (e) => {
+        e.preventDefault()
+
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+           setMensaje("No es un presupuesto válido")
+        } else {
+            //
+        }
+    }
 
   return (
     <div className='contenedor-presupuesto sombra'>
         
-        <form className='formulario'>
+        <form onSubmit={handlePresupuesto} className='formulario'>
             <div className='campo'>
                 <label>
                     Nuevo presupuesto
@@ -16,6 +27,7 @@ function NuevoPresupuesto({presupuesto, setPresupuesto}) {
                 onChange={e=>{setPresupuesto(e.target.value)}}/>
             </div>
             <input type='submit' value='añadir' />
+            {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
         </form>
     </div>
   )
