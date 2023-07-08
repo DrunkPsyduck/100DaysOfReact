67cee06e74481fd23f5cf4bdca9c173dccca092f
DrunkPsyduck <canalessanchezmario@gmail.com>
2023-07-08 11:34:07 +0200

    se añade funcionalidad para añadir un diccinari de gastos para mostrar los iconos

--- control-gastos/src/Components/Gasto.jsx
+++ control-gastos/src/Components/Gasto.jsx
@@ -1,23 +1,42 @@
+import {formatearFecha} from './helpers'
+import IconoAhorro from '../img/icono_ahorro.svg'
+import IconoCasa from '../img/icono_casa.svg'
+import IconoComida from '../img/icono_comida.svg'
+import IconoGastos from '../img/icono_gastos.svg'
+import IconoOcio from '../img/icono_ocio.svg'
+import IconoSalud from '../img/icono_salud.svg'
+import IconoSuscripciones from '../img/icono_suscripciones.svg'
+
+const diccionarioIconos = {
+    ahorro: IconoAhorro,
+    comida: IconoComida,
+    casa: IconoCasa,
+    gastos: IconoGastos,
+    ocio: IconoOcio,
+    salud: IconoSalud,
+    suscripciones: IconoSuscripciones
+}
 
 
 const Gasto =({key, gasto})=> {
-    const {categoria, nombre, cantidad, id} = gasto
+    const {categoria, nombre, cantidad, id, fecha} = gasto
     console.log(gasto)
   return (
     <div className="gasto sombra">
-        <div className="contenido-gasto">
-            <div className="descripcion-gasto">
-                <a className="categoria">
-                    {categoria}
-                </a>
-                <a className="categoria">
-                    {nombre}{gasto.nombre}
-                </a>
-            </div>
+      <div className="contenido-gasto">
+        <img src={diccionarioIconos[categoria]} alt="Icono" />
+        <div className="descripcion-gasto">
+          <a className="categoria">{categoria}</a>{""}
+          <a className="nombre-gasto">{nombre}</a>
+          <p className="fecha-gasto">
+            {" "}
+            Agregado el: <span>{formatearFecha(fecha)}</span>
+          </p>
         </div>
-
+      </div>
+      <p className="cantidad-gasto">{cantidad}</p>
     </div>
-  )
+  );
 }
 
 
